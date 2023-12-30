5499a6f02cd5b4d0695e9ee5484f452694df456c
Hoang Nguyen <hoangncd3@gmail.com>
2023-12-30 23:21:11 +0700

    feat: add form textarea

---
+++ src/components/common/form/form-textarea.tsx
@@ -0,0 +1,50 @@
+import { ChangeEventHandler } from 'react'
+
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
+
+type Props = {
+  label?: string
+  placeholder?: string
+  className?: string
+  description?: string
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>
+  value?: string
+  disabled?: boolean
+}
+
+const FormTextarea = ({
+  label = '',
+  placeholder = '',
+  className,
+  description,
+  onChange,
+  value,
+  disabled = false,
+}: Props) => {
+  return (
+    <FormItem>
+      <FormLabel>{label}</FormLabel>
+      <FormControl>
+        <Textarea
+          placeholder={placeholder}
+          className={cn(className)}
+          onChange={onChange}
+          value={value}
+          disabled={disabled}
+        />
+      </FormControl>
+      <FormDescription>{description}</FormDescription>
+      <FormMessage />
+    </FormItem>
+  )
+}
+
+export default FormTextarea
