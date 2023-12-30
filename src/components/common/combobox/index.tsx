2fb2bee8cf939f8174738fad2dbc8c6e08261be1
Hoang Nguyen <hoangncd3@gmail.com>
2023-12-30 23:08:43 +0700

    feat: add combobox

---
+++ src/components/common/combobox/index.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { Check, ChevronsUpDown } from 'lucide-react'
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from '@/components/ui/command'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { SelectOptions } from '@/components/ui/select/type'
+import { cn } from '@/lib/utils'
+
+type Props = {
+  options: SelectOptions[]
+  value?: string
+  onChange?: (value: string) => void
+  placeholder?: string
+  className?: string
+}
+
+const Combobox = ({
+  options = [],
+  onChange,
+  value,
+  placeholder = 'Select',
+  className = '',
+}: Props) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className={cn('w-full justify-between', className)}
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : placeholder}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <Command>
+          <CommandInput placeholder="Search..." />
+          <CommandEmpty>Not found.</CommandEmpty>
+          <CommandGroup>
+            {options.map((option) => (
+              <CommandItem
+                disabled={option.disabled}
+                key={option.value}
+                value={option.value}
+                onSelect={onChange}
+              >
+                <Check
+                  className={cn(
+                    'mr-2 h-4 w-4',
+                    value === option.value ? 'opacity-100' : 'opacity-0',
+                  )}
+                />
+                {option.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default Combobox
