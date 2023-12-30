255e01e98d593809b8c048d049e56f6e92544c4b
Hoang Nguyen <hoangncd3@gmail.com>
2023-12-30 17:27:38 +0700

    feat: add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { PropsWithChildren } from 'react'
 
+import { Toaster } from '@/components/ui/sonner'
 import { poppinsFont } from '@/configs'
 
 export const metadata: Metadata = {
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={poppinsFont.className}>{children}</body>
+      <body className={poppinsFont.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   )
 }
