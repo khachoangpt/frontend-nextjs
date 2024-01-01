e6b9368d20e2de17a4d0d89a138ca562f08a0edb
Hoang Nguyen <hoangncd3@gmail.com>
2024-01-01 21:32:23 +0700

    feat: add checkbox form

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,11 @@
 import { getTranslations } from 'next-intl/server'
 
-import Checkbox from '@/components/common/checkbox'
 import Container from '@/components/common/container'
 
 const HomePage = async () => {
   const t = await getTranslations('HomePage')
-  return (
-    <Container>
-      {t('home')}
-      <Checkbox label="Accept terms and conditions" id="java" />
-    </Container>
-  )
+
+  return <Container>{t('home')}</Container>
 }
 
 export default HomePage
