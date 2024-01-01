a165cb100dedf2aea2f761b6cf92e2e3d2477def
Hoang Nguyen <hoangncd3@gmail.com>
2024-01-01 15:19:52 +0700

    feat: add aspect ratio

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,7 @@
-import { getTranslations } from 'next-intl/server'
-
 import Container from '@/components/common/container'
 
 const HomePage = async () => {
-  const t = await getTranslations('Common')
-  return <Container>{t('content')}</Container>
+  return <Container></Container>
 }
 
 export default HomePage
