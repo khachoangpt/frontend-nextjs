2889d2fa0247b5e8d48eaeee536d1fd6433a020f
Hoang Nguyen <hoangncd3@gmail.com>
2024-01-02 22:56:51 +0700

    fix: remove default locale in url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,42 @@
-import createMiddleware from 'next-intl/middleware'
+import { NextRequest } from 'next/server'
+import createIntlMiddleware from 'next-intl/middleware'
 
 import { locales } from './i18n'
 
-export default createMiddleware({
-  locales,
-  defaultLocale: locales[0],
-})
+export default async function middleware(request: NextRequest) {
+  const defaultLocale = locales[0]
+  const locale = request.cookies.get('NEXT_LOCALE')
+  const pathname = request.nextUrl.pathname
+
+  const pathnameIsMissingLocale = locales.every(
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  )
+
+  const handleI18nRouting = createIntlMiddleware({
+    locales,
+    defaultLocale,
+  })
+  if (pathnameIsMissingLocale) {
+    if (locale?.value === defaultLocale) {
+      request.nextUrl.pathname = `/${locale?.value}${
+        pathname.startsWith('/') ? '' : '/'
+      }${pathname}`
+    }
+  }
+
+  const response = handleI18nRouting(request)
+
+  // Step 3: Alter the response (example)
+  response.headers.set('x-your-custom-locale', defaultLocale)
+
+  return response
+}
 
 export const config = {
-  matcher: ['/', '/(vi|en)/:path*'],
+  matcher: [
+    '/',
+    '/(vi|en)/:path*',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
